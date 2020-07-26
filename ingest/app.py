91f134dce4f1b1143af1f99aa6a53547a9eb1282
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-07-25 17:13:38 -0700

    removed needless deployments

--- ingest/app.py
+++ ingest/app.py
@@ -7,22 +7,16 @@ app = Flask("ingest")
 
 @app.route('/ingest', methods=["POST"])
 def hello_world():
-    body = json.loads(request.json['val'])
-    pprint.pprint(body['resource'])
-    for i in body['instrumentation_library_spans']:
-        for span in i['spans']:
-            pprint.pprint(span)
-            pprint.pprint("---------------------")
-
-    pprint.pprint("========================")
-    return 'OK', 200
+    pprint.pprint(json.loads(request.data))
+    pprint.pprint("\n\n")
+    return jsonify({"success": "ok"})
 
 
 @app.route('/v2/data', methods=["POST"])
 def metrics():
     pprint.pprint(request.json)
-    pprint.pprint("========================")
-    return jsonify({"sucess": "ok"})
+    pprint.pprint("\n\n")
+    return jsonify({"success": "ok"})
 
 
 if __name__ == '__main__':
