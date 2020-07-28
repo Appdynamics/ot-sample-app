879793a221784df3ce07b725d5d3918ca06431e0
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-07-28 13:28:47 -0700

    fixed resource

--- bookapi/app.py
+++ bookapi/app.py
@@ -28,7 +28,10 @@ def hello_world(username):
 
 
 if __name__ == '__main__':
-    trace.set_tracer_provider(TracerProvider(resource=Resource({"k8.cluster": "dev"})))
+    trace.set_tracer_provider(TracerProvider(resource=Resource({"k8.cluster": "dev",
+                                                                "appd.application": "bookings",
+                                                                "appd.service": "gateway"
+                                                                })))
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--exporter", type=str, choices=["appd", "otc", "jaeger", "console"],
                         help="choose exporter", default="jaeger")
