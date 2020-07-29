c401a88f8a1cf885d6c0447629c8f39d4622a9e7
Kanika Shah <kanikashah90@gmail.com>
2020-07-29 11:43:49 -0700

    Replacing the Python Booking Service with Node Booking Service

--- booksvc/ot_install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/open-telemetry/opentelemetry-python.git
-./opentelemetry-python/scripts/build.sh
-DISTDIR=opentelemetry-python/dist
-pip install $DISTDIR/opentelemetry-api-0.11.dev0.tar.gz $DISTDIR/opentelemetry-sdk-0.11.dev0.tar.gz $DISTDIR/opentelemetry-instrumentation-0.11.dev0.tar.gz $DISTDIR/opentelemetry-ext-wsgi-0.11.dev0.tar.gz $DISTDIR/opentelemetry-ext-flask-0.11.dev0.tar.gz $DISTDIR/opentelemetry-ext-requests-0.11.dev0.tar.gz $DISTDIR/opentelemetry-ext-jaeger-0.11.dev0.tar.gz $DISTDIR/opentelemetry-proto-0.11.dev0.tar.gz  $DISTDIR/opentelemetry-ext-otlp-0.11.dev0.tar.gz $DISTDIR/opentelemetry-ext-wsgi-0.11.dev0.tar.gz && cd ..
