edaad780b3d2ecf844d53e937a34581cf1007e09
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-09-20 16:16:33 -0700

    further clean up

--- bookapi/app.py
+++ bookapi/app.py
@@ -28,21 +28,6 @@ def hello_world(username):
 
 
 if __name__ == '__main__':
-    trace.set_tracer_provider(TracerProvider(resource=Resource({"k8.cluster": "dev",
-                                                                "service.name": "gateway"
-                                                                })))
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--exporter", type=str, choices=["appd", "otc", "jaeger", "jaeger"],
-                        help="choose exporter", default="jaeger")
-    args = parser.parse_args()
-
-    if args.exporter == "otc":
-        exporter = OTLPSpanExporter(os.getenv("OTC_HOST"))
-    elif args.exporter == "console":
-        exporter = ConsoleSpanExporter()
-    else:
-        exporter = JaegerSpanExporter(service_name=app.name,
-                                      agent_host_name=os.getenv('JAEGER_HOST'))
-
-    trace.get_tracer_provider().add_span_processor(BatchExportSpanProcessor(exporter))
+    trace.set_tracer_provider(TracerProvider(resource=Resource({"service.name": "gateway"})))
+    trace.get_tracer_provider().add_span_processor(BatchExportSpanProcessor(OTLPSpanExporter(os.getenv("OTC_HOST"))))
     app.run(debug=True, host='0.0.0.0')
