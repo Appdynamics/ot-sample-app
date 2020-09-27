e2f5cd83b69d940f7c8ac31a8d82f927fb5ce933
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-09-27 11:51:45 -0700

    use tracer_provider and meter_provider from env vars

--- bookapi/app.py
+++ bookapi/app.py
@@ -4,14 +4,14 @@ import datetime
 import os
 
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider, Resource
+from opentelemetry.sdk.trace import Resource
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
 
 from opentelemetry import metrics
-from opentelemetry.sdk.metrics import  MeterProvider
 from opentelemetry.sdk.metrics.export import ConsoleMetricsExporter
+from opentelemetry.exporter.otlp.metrics_exporter import OTLPMetricsExporter
 
 app = Flask("api")
 
@@ -33,11 +33,10 @@ def hello_world(username):
 if __name__ == '__main__':
     resource = Resource({"service.name": "gateway"})
 
-    trace.set_tracer_provider(TracerProvider(resource=resource))
+    trace.get_tracer_provider().resource = resource
     trace.get_tracer_provider().add_span_processor(BatchExportSpanProcessor(OTLPSpanExporter(os.getenv("OTC_HOST"))))
 
-    metrics.set_meter_provider(MeterProvider(resource=resource))
-    print(metrics.get_meter_provider())
+    metrics.get_meter_provider().resource = resource
     metrics.get_meter_provider().start_pipeline(RequestsInstrumentor().meter, ConsoleMetricsExporter(), 1)
 
     app.run(debug=True, host='0.0.0.0')
