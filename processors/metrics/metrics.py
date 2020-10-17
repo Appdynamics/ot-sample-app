523ae2e619e8ccbc850ddb00c4bf81409a8da2fb
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-10-16 18:08:30 -0700

    processors

---
+++ processors/metrics/metrics.py
@@ -0,0 +1,17 @@
+import redis
+import os
+import logging
+
+r = redis.Redis.from_url("redis://"+os.getenv("REDIS_ENDPOINT"))
+
+
+def subscribe(channel: str) -> None:
+    """Process messages from the pubsub stream."""
+    ps = r.pubsub()
+    ps.subscribe(channel)
+    for raw_message in ps.listen():
+        logging.info(raw_message)
+
+
+if __name__ == '__main__':
+    subscribe(os.getenv("REDIS_METRICS_CHANNEL"))
