3232e1ee82a8f0d0d9f2838c706ab55520f66963
Pavan Krishna <pavan.krishna@appdynamics.com>
2020-10-16 23:34:24 -0700

    nice stuff

--- processors/metrics/metrics.py
+++ processors/metrics/metrics.py
@@ -1,9 +1,15 @@
 import redis
 import os
-import json
 import pprint
-from google.protobuf.json_format import Parse
 from opentelemetry.proto.collector.metrics.v1 import metrics_service_pb2 as mspb
+
+
+def process_metrics(message: mspb.ExportMetricsServiceRequest) -> None:
+    """
+
+    """
+    pass
+
 
 if __name__ == '__main__':
     r = redis.Redis.from_url("redis://" + os.getenv("REDIS_ENDPOINT"))
@@ -11,9 +17,8 @@ if __name__ == '__main__':
     ps.subscribe(os.getenv("REDIS_METRICS_CHANNEL"))
     for raw_message in ps.listen():
         try:
-            print('here')
             message = mspb.ExportMetricsServiceRequest()
-            Parse(raw_message['data'], message, True)
+            message.ParseFromString(raw_message['data'])
             pprint.pprint(message)
-        except (json.decoder.JSONDecodeError, TypeError, AttributeError):
+        except (AttributeError,TypeError):
             print('failed')
